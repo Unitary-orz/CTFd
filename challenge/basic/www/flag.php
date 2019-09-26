74fb724d0729d9ccb21ba2b8f36f8655ddeb4a51
youyouj <792136506@qq.com>
2019-09-26 11:53:30 +0800

    add "basic" series challenges

---
+++ challenge/basic/www/flag.php
@@ -0,0 +1,11 @@
+<?php 
+
+$flag1 = "flag{03fdc0ee2rc464b";
+$flag2 = "c3c40ef0e20dcb5a}";
+$flag3 = "flag{03fdc0ef2fc464aac3c40ef0e25dcb5a}";
+$flag4 = "flag{hnpww7ty70fyi8k2ruiki9in5x2bunj}";
+$flag5 = "flag{6g6pg0c2pc2spbojkvairqb7dx7iide}";
+$flag6 = "flag{jwcgfgwset7bf1oe1xk2yhc3z66wsa6";
+$flag7 = "flag{x2mq7ntophi1pwucoy3dhptb1nda163}";
+$flag8 = "flag{csgol9t2mwsqzcn9gx4rf5notlwnu4f}";
+?>
