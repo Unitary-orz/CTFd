b71725fef3c21e552e5a0fe9d0624fa8d8a4c564
youyouj <792136506@qq.com>
2019-09-30 11:18:46 +0800

    fix strpos of challenge

--- challenge/Code-Auditing/basic/www/strpos/strpos.php
+++ challenge/Code-Auditing/basic/www/strpos/strpos.php
@@ -1,8 +1,8 @@
 <?php
 show_source(__FILE__);
-$flag7='in ../flag.php';
+$flag8='flag in ../flag.php';
 $filename = $_GET['file'];
 if(strpos($filename, 'flag') != false) die("非法字符");
 @include "./$filename";
 echo $flag8;
-?> 
+?>
--- challenge/Code-Auditing/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pyhton3
-# -*- coding:utf-8 -*-
-
-import requests
-
-url = "http://127.0.0.1:8094/?file=php://input&name=1.php&data=<?php phpinfo();"
-rep = requests.post(url, data='Hello World')
-print(rep.text)
