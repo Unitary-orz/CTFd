c702683b3784962f477095f3cc018e5d404afefc
Unitary-orz <792136506@qq.com>
2020-03-23 14:54:44 +0800

    add "qiandao","maxlen" challenge for Web

---
+++ Web/Code-Auditing/basic/www/qiandao.php
@@ -0,0 +1,13 @@
+$hello=$_GET['hello'];<br>
+echo $hello;<br>
+if($hello=='flag')<br>
+echo 'flag{****}';<br>
+
+
+<?php
+$hello=$_GET['hello'];
+echo $hello;
+if($hello=='flag')
+
+echo 'flag{hellophp}';
+?>
---
+++ Web/Code-Auditing/eval/www/flag.php
@@ -0,0 +1,5 @@
+<?php
+	$flag = 'Too Young Too Simple';
+	# echo $flag;
+	# flag{kibaoh9cvgza9v3spbw8ov4lmu4z5zd}
+?>
